c095fd8964bef1a9b31047e1501aeb8011471dce
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2024-12-05 03:44:12 +0200

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  let newArray = [];
+  let found = false;
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < remove.length; j++){
+      if(array[i] === remove[j]){
+        found = true;
+        break;
+      }
+    }
+    if (!found){
+      newArray.push(array[i]);
+    }
+    // reset found flag
+    found = false;
+  }
+  return newArray;
 };
 
 // Do not edit below this line
