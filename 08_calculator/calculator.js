73d0e8766bd5870f6a6d790fc88e70460e71ac4a
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2024-12-08 03:07:56 +0200

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+  for(let i = 0; i < a.length; i++){
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1;
+  for(let i = 0; i < a.length; i++){
+    result *= a[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  if(a == 0 || a == 1){
+    return result;
+  }
+  else{
+    for(let i = a; i >= 1; i--){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
