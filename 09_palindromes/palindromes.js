46ee797dcebb0c14fb22c2e77bbd4efbdc739203
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2024-12-09 05:54:33 +0200

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const punctuationRegex = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/g;
+  text = text.toLowerCase();
+  console.log(text)
+  // remove punctuation
+  text = text.replace(punctuationRegex, '');
+  // remove spaces
+  text = text.replaceAll(" ", "");
+  console.log(text);
 
+  let reversed = "";
+  for(i = text.length - 1; i >= 0; i--){
+    reversed += text[i];
+  }
+  if(text == reversed){
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
