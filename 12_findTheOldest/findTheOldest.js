18106da1b07658775dc74327aacd52f7178136ee
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2024-12-09 06:43:29 +0200

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let currentYear = new Date().getFullYear();
+  let ages = [];
+  let age;
 
+  // calculate ages
+  for(let i = 0; i < people.length; i++){
+    // if true use current year
+    if(people[i]["yearOfDeath"] == undefined){
+      age = currentYear - people[i]["yearOfBirth"];
+    }
+    else{
+      age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+    }
+    ages.push(age);
+  }
+
+  // now find the largest
+  let oldestIndex = 0;
+  let oldest = ages[oldestIndex];
+  for(let i = 1; i < ages.length; i++){
+    if(ages[i] > oldest){
+      oldest = ages[i];
+      oldestIndex = i;
+    }
+  }
+
+  return people[oldestIndex];
 };
 
 // Do not edit below this line
